ca18783b1bff6925ff3ba33a29d5b562eebdb234
ziprangga <ziprangga@gmail.com>
2026-01-12 18:28:12 +0700

    refactor structure file tree

--- app/src/app_modal.rs
+++ app/src/app_modal.rs
@@ -30,31 +30,67 @@ pub fn modal_process_kill_dialog(app_name: &str) -> Result<bool> {
 // use objc2_app_kit::NSAlert;
 // use objc2_foundation::NSString;
 
-// pub fn modal_kill_dialog(app_name: &str) -> bool {
-//     autoreleasepool(|_pool| unsafe {
-//         // Allocate + init NSAlert
-//         let alert: *mut NSAlert = msg_send![NSAlert::class(), alloc];
-//         let alert: *mut NSAlert = msg_send![alert, init];
+// pub fn modal_process_kill_dialog(app_name: &str) -> Result<bool, String> {
+//     // autoreleasepool(|_pool| unsafe {
+//     //     // Allocate + init NSAlert
+//     //     let alert: *mut NSAlert = msg_send![NSAlert::class(), alloc];
+//     //     let alert: *mut NSAlert = msg_send![alert, init];
 
-//         // Set message text
-//         let msg = NSString::from_str(&format!("The app '{}' is still running.", app_name));
-//         let info = NSString::from_str(
-//             "Do you want to kill its running process?\nBe careful to save your work first!",
-//         );
+//     //     // Set message text
+//     //     let msg = NSString::from_str(&format!("The app '{}' is still running.", app_name));
+//     //     let info = NSString::from_str(
+//     //         "Do you want to kill its running process?\nBe careful to save your work first!",
+//     //     );
 
-//         let _: () = msg_send![alert, setMessageText: &*msg];
-//         let _: () = msg_send![alert, setInformativeText: &*info];
+//     //     let _: () = msg_send![alert, setMessageText: &*msg];
+//     //     let _: () = msg_send![alert, setInformativeText: &*info];
 
-//         // Add buttons
-//         let yes = NSString::from_str("Yes");
-//         let no = NSString::from_str("No");
-//         let _: () = msg_send![alert, addButtonWithTitle: &*yes];
-//         let _: () = msg_send![alert, addButtonWithTitle: &*no];
+//     //     // Add buttons
+//     //     let yes = NSString::from_str("Yes");
+//     //     let no = NSString::from_str("No");
+//     //     let _: () = msg_send![alert, addButtonWithTitle: &*yes];
+//     //     let _: () = msg_send![alert, addButtonWithTitle: &*no];
 
-//         // Run modally
-//         let response: i32 = msg_send![alert, runModal];
+//     //     // Run modally
+//     //     let response: i32 = msg_send![alert, runModal];
 
-//         // 1000 = first button return (Yes)
-//         response == 1000
-//     })
+//     //     // 1000 = first button return (Yes)
+//     //     response == 1000
+//     // })
+//     // =====================
+//     let result = std::panic::catch_unwind(|| {
+//         autoreleasepool(|_pool| unsafe {
+//             // Allocate + init NSAlert
+//             let alert: *mut NSAlert = msg_send![NSAlert::class(), alloc];
+//             let alert: *mut NSAlert = msg_send![alert, init];
+
+//             // Set message text
+//             let msg = NSString::from_str(&format!("The app '{}' is still running.", app_name));
+//             let info = NSString::from_str(
+//                 "Do you want to kill its running process?\nBe careful to save your work first!",
+//             );
+
+//             let _: () = msg_send![alert, setMessageText: &*msg];
+//             let _: () = msg_send![alert, setInformativeText: &*info];
+
+//             // Add buttons
+//             let yes = NSString::from_str("Yes");
+//             let no = NSString::from_str("No");
+//             let _: () = msg_send![alert, addButtonWithTitle: &*yes];
+//             let _: () = msg_send![alert, addButtonWithTitle: &*no];
+
+//             // Run modally
+//             let response: i32 = msg_send![alert, runModal];
+
+//             // 1000 = first button return (Yes)
+//             response == 1000
+//         })
+//     });
+
+//     result.map_err(|_| "Failed to display NSAlert dialog".to_string())
 // }
+
+// result:
+// thread 'main' (65550) panicked at app/src/app_modal.rs:78:25:
+// invalid message send to -[NSAlert addButtonWithTitle:]: expected return to have type code '@', but found 'v'
+// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
--- cleaner/src/foundation.rs
+++ cleaner/src/foundation.rs
@@ -1,9 +1,9 @@
-mod c_sys_conf;
-mod ns_bom_log;
-mod ns_file_manager;
-mod ns_kill_process;
+mod sys_bom_log;
+mod sys_conf_path;
+mod sys_file_manager;
+mod sys_kill_process;
 
-pub use c_sys_conf::*;
-pub use ns_bom_log::*;
-pub use ns_file_manager::*;
-pub use ns_kill_process::*;
+pub use sys_bom_log::*;
+pub use sys_conf_path::*;
+pub use sys_file_manager::*;
+pub use sys_kill_process::*;
