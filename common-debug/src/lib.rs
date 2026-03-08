52944eacd172d6d771e9c0fd0af4d7a2da73a2df
ziprangga <ziprangga@gmail.com>
2026-03-08 08:46:59 +0700

    Refactor common-debug to improve log macros: - Make macros proper wrappers around the log crate. - Enable or disable logging via features. - Remove debug_assertions gating so debug can optionally be used in release builds. - debug_assertions can still be combined in the caller or with init_dev_logger in main.

--- common-debug/src/lib.rs
+++ common-debug/src/lib.rs
@@ -2,11 +2,10 @@ pub use log;
 // -------------------------------
 // Debug-only Logger Init
 // -------------------------------
-#[cfg(all(debug_assertions, feature = "debug-dev"))]
+#[cfg(feature = "debug-dev")]
 pub fn init_dev_logger() {
     // Initialize logger; ignore errors if already initialized
     let _ = env_logger::builder()
-        // .filter_level(log::LevelFilter::Info)
         .filter_module("cleaner", log::LevelFilter::Debug)
         .filter_module("widget", log::LevelFilter::Debug)
         .filter_module("Bristo", log::LevelFilter::Debug)
@@ -14,21 +13,24 @@ pub fn init_dev_logger() {
 }
 
 #[cfg(not(feature = "debug-dev"))]
-pub fn init_dev_logger() {
-    // no-op
-}
+pub fn init_dev_logger() {}
 
 // -------------------------------
 // Debug-only macro
 // -------------------------------
 
+#[cfg(feature = "debug-dev")]
 #[macro_export]
 macro_rules! debug_dev {
     ($($arg:tt)*) => {
-        #[cfg(debug_assertions)]
-        // #[cfg(all(debug_assertions, feature = "debug-dev"))]
         {
             $crate::log::debug!($($arg)*);
         }
     };
+}
+
+#[cfg(not(feature = "debug-dev"))]
+#[macro_export]
+macro_rules! debug_dev {
+    ($($arg:tt)*) => {};
 }
