8a74f70ce4fc8499d912b24e23dc4436ccfeec7c
ziprangga <ziprangga@gmail.com>
2026-07-07 20:30:59 +0700

    rename sys_bom_log to sys_utility to prepare the module contents as a collection of system utilities

--- cleaner/src/syscom/mod.rs
+++ cleaner/src/syscom/mod.rs
@@ -60,19 +60,17 @@
 //! All APIs in this module are currently macOS-specific.
 //!..
 
-mod sys_bom_log;
-mod sys_component;
-
-pub use sys_bom_log::run_lsbom_command;
-pub use sys_component::{
-    DARWIN_USER_CACHE_DIR, DARWIN_USER_TEMP_DIR, kill_pids, show_in_finder, sysconf_path,
-    trash_files_nsfilemanager,
-};
-
-// =============================
-
 mod sys_asset;
+mod sys_component;
+mod sys_utility;
+
+// pub use sys_access::run_cmd_as_root;
 pub use sys_asset::{
     get_default_file_icon, get_default_folder_icon, get_installed_app_icon_by_path,
     ns_image_to_rgba_bytes,
 };
+pub use sys_component::{
+    DARWIN_USER_CACHE_DIR, DARWIN_USER_TEMP_DIR, kill_pids, show_in_finder, sysconf_path,
+    trash_files_nsfilemanager,
+};
+pub use sys_utility::run_lsbom_command;
